2457892ca188342ad09c9f34a3d012ad2350439b
Paula-Adriana <paulaadrianadacosta@gmail.com>
2021-12-06 08:36:32 -0300

    Exercicios

---
+++ Dezembro/Exercicios/src/EscolaFuturo.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+/* A Escola Futuro precisa controlar as notas e médias dos seus alunos.
+Para isso precisa de um programa que faça o seguinte:
+1 - Informar a quantidade de alunos
+2 - Informar a quantidade de notas
+3 - Informar nota a nota
+4 - Calcular a média 
+5 - Para cada aluno verificar a situação
+Se média < 6 - Reprovado
+Se média >= 6 e < 7.5 - Em Recuperação
+Se média >= 7.5 - Aprovado
+6 - Para cada aluno imprimir o Nome, Média e Situação
+7 - No final do programa imprimir a melhor média e a média da turma
+*/
+public class EscolaFuturo {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		String situacao, nome;
+		double somaNota = 0, media = 0, mediaTurma = 0, melhorMedia = 0;
+		int[] numeroNotas = new int[10];
+
+		System.out.print("Informe a quantidade de alunos: ");
+		int quantidadeAlunos = entrada.nextInt();
+
+		for (int i = 1; i <= quantidadeAlunos; i++) {
+			System.out.print("Informe o nome do aluno: ");
+			nome = entrada.next();
+			
+			System.out.print("Informe a quantidade de notas: ");
+			int quantidadeNotas = entrada.nextInt();
+
+			for (int j = 0; j < quantidadeNotas; j++) {
+				System.out.print("Informe a " + (j + 1) + "ª nota: ");
+				numeroNotas[j] = entrada.nextInt();
+				somaNota += numeroNotas[j];
+			}
+			media = somaNota / quantidadeNotas;
+			mediaTurma += media / quantidadeAlunos;
+
+			if (media < 6) {
+				situacao = "Reprovado!";
+			} else if (media < 7.5) {
+				situacao ="Em recuperação!";
+			} else {
+				situacao ="Aprovado!";
+			}
+			if (media > melhorMedia) {
+				melhorMedia = media;
+			}
+			System.out.println();
+			System.out.println("****************************");
+			System.out.println("Nome do aluno: " + nome);
+			System.out.printf("Média: %.2f \n", media);
+			System.out.println("Situação: " + situacao);
+			System.out.println();
+			System.out.println("****************************");	
+			media = 0;
+			somaNota = 0;
+		}
+		System.out.printf("A melhor média da turma foi: " + melhorMedia + ""
+				+ " e a média geral foi: %.2f",mediaTurma);
+		
+		entrada.close();
+	}
+
+}
--- Dezembro/Exercicios/src/MaiorMenor.java
+++ Dezembro/Exercicios/src/MaiorMenor.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
 
 public class MaiorMenor {
-// Programa para que o usuário informe 10 números e ao final veja qual o menor e maior número digitado.
-	
+     // Programa para que o usuário informe 10 números e ao final veja qual
+	// o menor e maior número digitado.
+
 	public static void main(String[] args) {
-		
-		
-		int [] numero = new int[10];
+
+		int[] numero = new int[10];
 		int menor = 0, maior = 0;
 		Scanner entrada = new Scanner(System.in);
-		
-		for(int i = 0; i < numero.length; i++){
-			System.out.print("Digite o " +(i+1)+"º numero: ");
+
+		for (int i = 0; i < numero.length; i++) {
+			System.out.print("Digite o " + (i + 1) + "º numero: ");
 			numero[i] = entrada.nextInt();
-			
+
 			if (i == 0) {
-                maior = numero[i];
-                menor = numero[i];
-            }
-			
-			if(numero[i] > maior){ 
+				maior = numero[i];
+				menor = numero[i];
+			}
+
+			if (numero[i] > maior) {
 				maior = numero[i];
 			} else if (numero[i] < menor) {
 				menor = numero[i];
 			}
 		}
+		System.out.println();
 		System.out.println("O menor número digitado foi: " + menor + " e o maior foi: " + maior);
-				
+
 		entrada.close();
 
 	}
